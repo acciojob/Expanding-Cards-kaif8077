43cdcd101996c3f8ee47ed3e4835a8511ea0d459
Mohammad Kaif <149231751+kaif8077@users.noreply.github.com>
2026-01-22 00:51:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const panels = document.querySelectorAll(".panel");
 
-panels.forEach((panel) => {
+// create text for default active
+setActiveText(document.querySelector(".panel.active"));
+
+panels.forEach(panel => {
   panel.addEventListener("click", () => {
-    removeActiveClasses();
+    panels.forEach(p => p.classList.remove("active"));
     panel.classList.add("active");
+    setActiveText(panel);
   });
 });
 
-function removeActiveClasses() {
-  panels.forEach((panel) => {
-    panel.classList.remove("active");
-  });
+function setActiveText(panel) {
+  // REMOVE all existing img-text nodes
+  document.querySelectorAll(".img-text").forEach(el => el.remove());
+
+  // ADD text ONLY for active panel
+  const h3 = document.createElement("h3");
+  h3.className = "img-text";
+  h3.innerText = panel.dataset.text;
+  panel.appendChild(h3);
 }
