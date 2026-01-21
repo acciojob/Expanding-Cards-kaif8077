008c0ad374ea0fdf1d00e7149c0905c5aea0fccb
Mohammad Kaif <149231751+kaif8077@users.noreply.github.com>
2026-01-22 00:56:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const panels = document.querySelectorAll(".panel");
+const textEl = document.querySelector(".img-text");
 
 panels.forEach(panel => {
   panel.addEventListener("click", () => {
     panels.forEach(p => p.classList.remove("active"));
     panel.classList.add("active");
+
+    // UPDATE THE FIRST (AND ONLY) img-text
+    textEl.innerText = panel.dataset.text;
   });
 });
